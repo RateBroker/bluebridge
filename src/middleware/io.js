4dc8c97e524c8836585a8eecd5be83fde38077e3
Steven French <steven@frenchy.nz>
2016-09-14 09:19:38 +1200

    comment out redis related code

--- src/middleware/io.js
+++ src/middleware/io.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const passportSocketIo  = require('passport.socketio');
-const redisStore        = require('../redis-store');
+// const passportSocketIo  = require('passport.socketio');
+// const redisStore        = require('../redis-store');
 
 module.exports = function (io) {
 
-  io.use(passportSocketIo.authorize({
-    key:      'connect.sid',
-    secret:   'we<3RateBroker',
-    store:    redisStore,
-    success: function (data, accept) {
-      accept();
-    },
-    fail: function (data, message, error, accept) {
-      if (error) {
-        accept(new Error(message));
-      }
-      accept();
-    }
-  }));
+  // io.use(passportSocketIo.authorize({
+  //   key:      'connect.sid',
+  //   secret:   'we<3RateBroker',
+  //   store:    redisStore,
+  //   success: function (data, accept) {
+  //     accept();
+  //   },
+  //   fail: function (data, message, error, accept) {
+  //     if (error) {
+  //       accept(new Error(message));
+  //     }
+  //     accept();
+  //   }
+  // }));
 
 };
