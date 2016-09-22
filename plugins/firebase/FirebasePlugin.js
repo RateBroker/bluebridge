1ef42170b3927433c1c996f090daac4009eedd24
Steven French <steven@frenchy.nz>
2016-09-23 09:31:34 +1200

    remove firebase injected dependancy

--- plugins/firebase/FirebasePlugin.js
+++ plugins/firebase/FirebasePlugin.js
@@ -1,45 +1,41 @@
 'use strict';
 
 const Plugin      = require('../../src/Plugin');
+const firebase = require('firebase')
 
 class FirebasePlugin extends Plugin {
 
-  constructor (bluebridge, firebase) {
+  constructor (bluebridge) {
     super(bluebridge);
-
-    this.firebase = firebase;
-  }
-
-  onIOMiddleware () {
-
   }
 
   onConnection (socket) {
     socket.auth = null;
   }
 
+  IOmiddleware (io) {
+  }
+
   auth (socket, firebaseToken) {
     // Deauth if firebaseUser not set
     if (!firebaseToken) {
       socket.firebaseUser = null;
-
-      return Promise.reject();
+      return Promise.resolve('No firebaseToken provided, deauthed');
     }
 
+    let auth = firebase.auth();
+
     //check the auth data sent by the client
-    return this.firebase
-      .auth()
-      .verifyIdToken(firebaseToken)
-      .then(firebaseUser => {
-        // set firebaseUser on the socket
-        socket.firebaseUser = firebaseUser;
-        if (firebaseUser.uid) {
-          socket.emit('auth');
-        } else {
-          socket.emit('deauth');
-        }
-        return Promise.resolve('Authenticated as ' + socket.firebaseUser.name);
-      });
+    return auth.verifyIdToken(firebaseToken).then(firebaseUser => {
+      // set firebaseUser on the socket
+      socket.firebaseUser = firebaseUser;
+      if (firebaseUser.uid) {
+        socket.emit('auth');
+      } else {
+        socket.emit('deauth');
+      }
+      return Promise.resolve('Authenticated as ' + socket.firebaseUser.name);
+    });
   }
 
   expose () {
